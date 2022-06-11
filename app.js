db9887cac4ebe68af248d451c6f312d752e54ccf
Jane Eyre <corimest@gmail.com>
2022-06-11 14:22:43 -0500

    user input validation

--- app.js
+++ app.js
@@ -15,12 +15,27 @@ const promptUser = () => {
     {
       type: 'input', 
       name: 'name',
-      message: 'What is your name?'
+      message: 'What is your name? (Required)',
+      validate: nameInput => {
+        if (nameInput) {
+          return true; 
+        } else {
+          console.log('Please enter your name!');
+          return false; 
+        }
+      }
     }, 
     {
       type: 'input', 
       name: 'github', 
-      message: 'Enter your GitHub Username'
+      message: 'Enter your GitHub Username (Required)', 
+      validate: githubInput => {
+        if (githubInput) {
+          return true; 
+        } else {
+          console.log('Please enter your GitHub Username!')
+        }
+      }
     }, 
     {
       type: 'input', 
@@ -49,7 +64,14 @@ const promptProject = portfolioData => {
     {
       type: 'input', 
       name: 'description', 
-      message: 'Provide a description of the project (Required)'
+      message: 'Provide a description of the project (Required)', 
+      validate: descriptInput => {
+        if (descriptInput) {
+          return true;
+        } else {
+          console.log ('Please enter a decription of your project!')
+        }
+      }
     },
     {
       type: 'checkbox', 
@@ -60,7 +82,14 @@ const promptProject = portfolioData => {
     {
       type: 'input', 
       name: 'link', 
-      message: 'Enter the GitHub link to your project. (Required)'
+      message: 'Enter the GitHub link to your project. (Required)',
+      validate: linkInput => {
+        if (linkInput) {
+          return true
+        } else {
+          console.log('Please enter a link to your GitHub project!')
+        }
+      }
     }, 
     {
       type: 'confirm', 
